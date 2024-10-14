353e42b0f6aae2f74372b69e642a5a8a23473d9d
Jonathan Miller <jon@jmill.dev>
2024-10-14 15:13:49 +0700

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,11 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
+      <script
+        defer
+        src="https://umami-tawny-theta.vercel.app/script.js"
+        data-website-id="2f689c74-a3c1-41af-b14a-010a28c4f01f"
+      ></script>
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
       </body>
