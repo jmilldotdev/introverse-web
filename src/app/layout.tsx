2854f475faaf8d5bd62cf5013a1cd6ae91fa2796
Jonathan Miller <jon@jmill.dev>
2024-10-14 15:14:37 +0700

    wrong analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <script
         defer
         src="https://umami-tawny-theta.vercel.app/script.js"
-        data-website-id="2f689c74-a3c1-41af-b14a-010a28c4f01f"
+        data-website-id="dd42565c-a839-4c36-9a21-974f88775741"
       ></script>
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
