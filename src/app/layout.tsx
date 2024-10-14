f89ada4deff00788b80c9ba07264a58bffe077d4
Jonathan Miller <jon@jmill.dev>
2024-10-14 15:19:54 +0700

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,11 +14,13 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
-      <script
-        defer
-        src="https://umami-tawny-theta.vercel.app/script.js"
-        data-website-id="dd42565c-a839-4c36-9a21-974f88775741"
-      ></script>
+      <head>
+        <script
+          defer
+          src="https://umami-tawny-theta.vercel.app/script.js"
+          data-website-id="dd42565c-a839-4c36-9a21-974f88775741"
+        ></script>
+      </head>
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
       </body>
